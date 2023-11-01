52dafc8bca404754bed4778bde6bb45b9e0ded70
officialBusiness <a634318935@126.com>
2023-11-01 10:18:40 +0800

    开始整理 Babylon.js-1.0.8 代码

--- Babylon.js/Babylon.js-1.0.8/Babylon/Materials/textures/babylon.videoTexture.js
+++ Babylon.js/Babylon.js-1.0.8/Babylon/Materials/textures/babylon.videoTexture.js
@@ -1,50 +1,50 @@
 ﻿var BABYLON = BABYLON || {};
 
 (function () {
-	BABYLON.VideoTexture = function (name, urls, size, scene, generateMipMaps) {
-		this._scene = scene;
-		this._scene.textures.push(this);
+    BABYLON.VideoTexture = function (name, urls, size, scene, generateMipMaps) {
+        this._scene = scene;
+        this._scene.textures.push(this);
 
-		this.name = name;
+        this.name = name;
 
-		this.wrapU = BABYLON.Texture.REPEAT_CLAMPMODE;
-		this.wrapV = BABYLON.Texture.REPEAT_CLAMPMODE;
+        this.wrapU = BABYLON.Texture.REPEAT_CLAMPMODE;
+        this.wrapV = BABYLON.Texture.REPEAT_CLAMPMODE;
 
-		this._texture = scene.getEngine().createDynamicTexture(size, generateMipMaps);
-		var textureSize = this.getSize();
+        this._texture = scene.getEngine().createDynamicTexture(size, generateMipMaps);
+        var textureSize = this.getSize();
 
-		this.video = document.createElement("video");
-		this.video.width = textureSize.width;
-		this.video.height = textureSize.height;
-		this.video.autoplay = true;
-		this.video.loop = true;
+        this.video = document.createElement("video");
+        this.video.width = textureSize.width;
+        this.video.height = textureSize.height;
+        this.video.autoplay = true;
+        this.video.loop = true;
 
-		var that = this;
-		this.video.addEventListener("canplaythrough", function () {
-			that._texture.isReady = true;
-		});
+        var that = this;
+        this.video.addEventListener("canplaythrough", function () {
+            that._texture.isReady = true;
+        });
 
-		this.video.preload = true;
-		urls.forEach(function (url) {
-			var source = document.createElement("source");
-			source.src = url;
-			that.video.appendChild(source);
-		});
-	   
-		this._lastUpdate = new Date();
-	};
+        this.video.preload = true;
+        urls.forEach(function (url) {
+            var source = document.createElement("source");
+            source.src = url;
+            that.video.appendChild(source);
+        });
+       
+        this._lastUpdate = new Date();
+    };
 
-	BABYLON.VideoTexture.prototype = Object.create(BABYLON.Texture.prototype);
+    BABYLON.VideoTexture.prototype = Object.create(BABYLON.Texture.prototype);
 
-	BABYLON.VideoTexture.prototype._update = function () {
-		var now = new Date();
+    BABYLON.VideoTexture.prototype._update = function () {
+        var now = new Date();
 
-		if (now - this._lastUpdate < 15) {
-			return false;
-		}
+        if (now - this._lastUpdate < 15) {
+            return false;
+        }
 
-		this._lastUpdate = now;
-		this._scene.getEngine().updateVideoTexture(this._texture, this.video);
-		return true;
-	};
+        this._lastUpdate = now;
+        this._scene.getEngine().updateVideoTexture(this._texture, this.video);
+        return true;
+    };
 })();
