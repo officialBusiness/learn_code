7253ef37d57b43c04439440e52cb33139d07cf6d
officialBusiness <a634318935@126.com>
2024-03-28 19:31:19 +0800

    更新 three 案例代码匹配 r51

---
+++ three.js/examples/module.js
@@ -0,0 +1,168 @@
+
+import "../source/src/textures/Texture.js";
+
+import "../source/src/core/Vector3.js";
+
+import "../source/src/core/Matrix4.js";
+
+import "../source/src/core/Object3D.js";
+
+import "../source/src/objects/Mesh.js";
+
+import "../source/src/objects/Line.js";
+
+import "../source/src/cameras/Camera.js";
+import "../source/src/cameras/OrthographicCamera.js";
+import "../source/src/cameras/PerspectiveCamera.js";
+
+import "../source/src/core/BufferGeometry.js";
+import "../source/src/core/Clock.js";
+import "../source/src/core/Color.js";
+import "../source/src/core/EventTarget.js";
+import "../source/src/core/Face3.js";
+import "../source/src/core/Face4.js";
+import "../source/src/core/Frustum.js";
+import "../source/src/core/Geometry.js";
+import "../source/src/core/Math.js";
+import "../source/src/core/Matrix3.js";
+import "../source/src/core/Matrix4.js";
+import "../source/src/core/Object3D.js";
+import "../source/src/core/Projector.js";
+import "../source/src/core/Quaternion.js";
+import "../source/src/core/Ray.js";
+import "../source/src/core/Rectangle.js";
+import "../source/src/core/Spline.js";
+import "../source/src/core/UV.js";
+import "../source/src/core/Vector2.js";
+import "../source/src/core/Vector3.js";
+import "../source/src/core/Vector4.js";
+import "../source/src/core/Vertex.js";
+
+import "../source/src/extras/animation/Animation.js";
+import "../source/src/extras/animation/AnimationHandler.js";
+import "../source/src/extras/animation/AnimationMorphTarget.js";
+import "../source/src/extras/animation/KeyFrameAnimation.js";
+
+import "../source/src/extras/cameras/CombinedCamera.js";
+import "../source/src/extras/cameras/CubeCamera.js";
+
+import "../source/src/extras/controls/FirstPersonControls.js";
+import "../source/src/extras/controls/FlyControls.js";
+import "../source/src/extras/controls/OrbitControls.js";
+import "../source/src/extras/controls/PathControls.js";
+import "../source/src/extras/controls/RollControls.js";
+import "../source/src/extras/controls/TrackballControls.js";
+
+import "../source/src/extras/core/Curve.js";
+import "../source/src/extras/core/CurvePath.js";
+import "../source/src/extras/core/Gyroscope.js";
+import "../source/src/extras/core/Path.js";
+import "../source/src/extras/core/Shape.js";
+
+import "../source/src/extras/geometries/CircleGeometry.js";
+import "../source/src/extras/geometries/ConvexGeometry.js";
+import "../source/src/extras/geometries/CubeGeometry.js";
+import "../source/src/extras/geometries/CylinderGeometry.js";
+import "../source/src/extras/geometries/ExtrudeGeometry.js";
+import "../source/src/extras/geometries/IcosahedronGeometry.js";
+import "../source/src/extras/geometries/LatheGeometry.js";
+import "../source/src/extras/geometries/OctahedronGeometry.js";
+import "../source/src/extras/geometries/ParametricGeometry.js";
+import "../source/src/extras/geometries/PlaneGeometry.js";
+import "../source/src/extras/geometries/PolyhedronGeometry.js";
+import "../source/src/extras/geometries/ShapeGeometry.js";
+import "../source/src/extras/geometries/SphereGeometry.js";
+import "../source/src/extras/geometries/TetrahedronGeometry.js";
+import "../source/src/extras/geometries/TextGeometry.js";
+import "../source/src/extras/geometries/TorusGeometry.js";
+import "../source/src/extras/geometries/TorusKnotGeometry.js";
+import "../source/src/extras/geometries/TubeGeometry.js";
+
+import "../source/src/extras/helpers/ArrowHelper.js";
+import "../source/src/extras/helpers/AxisHelper.js";
+import "../source/src/extras/helpers/CameraHelper.js";
+
+import "../source/src/extras/modifiers/SubdivisionModifier.js";
+
+import "../source/src/extras/objects/ImmediateRenderObject.js";
+import "../source/src/extras/objects/LensFlare.js";
+import "../source/src/extras/objects/MorphBlendMesh.js";
+
+import "../source/src/extras/renderers/plugins/DepthPassPlugin.js";
+import "../source/src/extras/renderers/plugins/LensFlarePlugin.js";
+import "../source/src/extras/renderers/plugins/ShadowMapPlugin.js";
+import "../source/src/extras/renderers/plugins/SpritePlugin.js";
+
+import "../source/src/extras/shaders/ShaderFlares.js";
+import "../source/src/extras/shaders/ShaderSprite.js";
+
+import "../source/src/extras/ColorUtils.js";
+import "../source/src/extras/FontUtils.js";
+import "../source/src/extras/GeometryUtils.js";
+import "../source/src/extras/ImageUtils.js";
+import "../source/src/extras/SceneUtils.js";
+import "../source/src/extras/ShaderUtils.js";
+
+import "../source/src/lights/Light.js";
+import "../source/src/lights/AmbientLight.js";
+import "../source/src/lights/DirectionalLight.js";
+import "../source/src/lights/HemisphereLight.js";
+import "../source/src/lights/PointLight.js";
+import "../source/src/lights/SpotLight.js";
+
+import "../source/src/loaders/Loader.js";
+import "../source/src/loaders/BinaryLoader.js";
+import "../source/src/loaders/GeometryLoader.js";
+import "../source/src/loaders/ImageLoader.js";
+import "../source/src/loaders/JSONLoader.js";
+import "../source/src/loaders/LoadingMonitor.js";
+import "../source/src/loaders/SceneLoader.js";
+import "../source/src/loaders/TextureLoader.js";
+
+import "../source/src/materials/Material.js";
+import "../source/src/materials/LineBasicMaterial.js";
+import "../source/src/materials/MeshBasicMaterial.js";
+import "../source/src/materials/MeshDepthMaterial.js";
+import "../source/src/materials/MeshFaceMaterial.js";
+import "../source/src/materials/MeshLambertMaterial.js";
+import "../source/src/materials/MeshNormalMaterial.js";
+import "../source/src/materials/MeshPhongMaterial.js";
+import "../source/src/materials/ParticleBasicMaterial.js";
+import "../source/src/materials/ParticleCanvasMaterial.js";
+import "../source/src/materials/ParticleDOMMaterial.js";
+import "../source/src/materials/ShaderMaterial.js";
+
+import "../source/src/objects/Bone.js";
+import "../source/src/objects/Line.js";
+import "../source/src/objects/LOD.js";
+import "../source/src/objects/Mesh.js";
+import "../source/src/objects/MorphAnimMesh.js";
+import "../source/src/objects/Particle.js";
+import "../source/src/objects/ParticleSystem.js";
+import "../source/src/objects/Ribbon.js";
+import "../source/src/objects/SkinnedMesh.js";
+import "../source/src/objects/Sprite.js";
+
+import "../source/src/renderers/renderables/RenderableFace3.js";
+import "../source/src/renderers/renderables/RenderableFace4.js";
+import "../source/src/renderers/renderables/RenderableLine.js";
+import "../source/src/renderers/renderables/RenderableObject.js";
+import "../source/src/renderers/renderables/RenderableParticle.js";
+import "../source/src/renderers/renderables/RenderableVertex.js";
+
+import "../source/src/renderers/CanvasRenderer.js";
+import "../source/src/renderers/WebGLRenderer.js";
+import "../source/src/renderers/WebGLRenderTarget.js";
+import "../source/src/renderers/WebGLRenderTargetCube.js";
+import "../source/src/renderers/WebGLShaders.js";
+
+import "../source/src/scenes/Fog.js";
+import "../source/src/scenes/FogExp2.js";
+import "../source/src/scenes/Scene.js";
+
+import "../source/src/textures/CompressedTexture.js";
+import "../source/src/textures/DataTexture.js";
+import "../source/src/textures/Texture.js";
+
+import "../libs/lil-gui/lil-gui.umd.min.js";
+
