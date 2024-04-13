6ccae255c92acbfa1d7144725f2548d2ea7f0ae8
officialBusiness <a634318935@126.com>
2024-04-13 18:11:24 +0800

    更新 three 案例代码匹配 r101

---
+++ three.js/examples/jsm/jsm.js
@@ -0,0 +1,9 @@
+
+
+
+export { MapControls } from '../../source/examples/jsm/controls/MapControls.js';
+export { OrbitControls } from '../../source/examples/jsm/controls/OrbitControls.js';
+export { TrackballControls } from '../../source/examples/jsm/controls/TrackballControls.js';
+
+export { GLTFLoader } from '../../source/examples/jsm/loaders/GLTFLoader.js';
+export { OBJLoader } from '../../source/examples/jsm/loaders/OBJLoader.js';
