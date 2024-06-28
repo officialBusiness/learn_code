5e18c1e5b32d1e6365c4a8d3d1f50caecf9e8e7e
officialBusiness <a634318935@126.com>
2024-06-28 20:00:28 +0800

    初步学习了解  OutlinePass ; 调整修改 outline_pass.html

--- three.js/examples/jsm/postprocessing/outline_pass/outline_pass_tools.js
+++ three.js/examples/jsm/postprocessing/outline_pass/outline_pass_tools.js
@@ -1,7 +1,7 @@
 
 import * as THREE from 'three';
 
-export default function createOutlinePassScene(){
+export function createOutlinePassScene(){
 
 	const colorArray = [0x00cbc8, 0x0081cb, 0x00cbb8, 0x00c1cb, 0xbacb00, 0x7c00cb, 0x7a00cb, 0xa300cb, 0x9800cb, 0x00cbbc, 0x97cb00, 0x4000cb, 0xabcb00, 0x9fcb00, 0x8700cb, 0xcbb400, 0xcb004c, 0x64cb00, 0x0091cb, 0x00cbb8];
 	const positionArray = [
@@ -69,4 +69,139 @@ export default function createOutlinePassScene(){
 	}
 
 	return group;
+}
+
+export class OutlinePassTools {
+	constructor(scene, camera, selectedObjects){
+
+		this.renderScene = scene;
+		this.renderCamera = camera;
+		this.selectedObjects = selectedObjects !== undefined ? selectedObjects : [];
+
+
+		this._visibilityCache = new Map();
+
+
+		this.textureMatrix = new THREE.Matrix4();
+	}
+
+	changeVisibilityOfSelectedObjects( bVisible ) {
+
+		const cache = this._visibilityCache;
+
+		function gatherSelectedMeshesCallBack( object ) {
+
+			if ( object.isMesh ) {
+
+				if ( bVisible === true ) {
+
+					object.visible = cache.get( object );
+
+				} else {
+
+					cache.set( object, object.visible );
+					object.visible = bVisible;
+
+				}
+
+			}
+
+		}
+
+		for ( let i = 0; i < this.selectedObjects.length; i ++ ) {
+
+			const selectedObject = this.selectedObjects[ i ];
+			selectedObject.traverse( gatherSelectedMeshesCallBack );
+
+		}
+
+	}
+
+	changeVisibilityOfNonSelectedObjects( bVisible ) {
+
+		const cache = this._visibilityCache;
+		const selectedMeshes = [];
+
+		function gatherSelectedMeshesCallBack( object ) {
+
+			if ( object.isMesh ) selectedMeshes.push( object );
+
+		}
+
+		for ( let i = 0; i < this.selectedObjects.length; i ++ ) {
+
+			const selectedObject = this.selectedObjects[ i ];
+			selectedObject.traverse( gatherSelectedMeshesCallBack );
+
+		}
+
+		function VisibilityChangeCallBack( object ) {
+
+			if ( object.isMesh || object.isSprite ) {
+
+				// only meshes and sprites are supported by OutlinePass
+
+				let bFound = false;
+
+				for ( let i = 0; i < selectedMeshes.length; i ++ ) {
+
+					const selectedObjectId = selectedMeshes[ i ].id;
+
+					if ( selectedObjectId === object.id ) {
+
+						bFound = true;
+						break;
+
+					}
+
+				}
+
+				if ( bFound === false ) {
+
+					const visibility = object.visible;
+
+					if ( bVisible === false || cache.get( object ) === true ) {
+
+						object.visible = bVisible;
+
+					}
+
+					cache.set( object, visibility );
+
+				}
+
+			} else if ( object.isPoints || object.isLine ) {
+
+				// the visibilty of points and lines is always set to false in order to
+				// not affect the outline computation
+
+				if ( bVisible === true ) {
+
+					object.visible = cache.get( object ); // restore
+
+				} else {
+
+					cache.set( object, object.visible );
+					object.visible = bVisible;
+
+				}
+
+			}
+
+		}
+
+		this.renderScene.traverse( VisibilityChangeCallBack );
+
+	}
+
+	updateTextureMatrix() {
+
+		this.textureMatrix.set( 0.5, 0.0, 0.0, 0.5,
+			0.0, 0.5, 0.0, 0.5,
+			0.0, 0.0, 0.5, 0.5,
+			0.0, 0.0, 0.0, 1.0 );
+		this.textureMatrix.multiply( this.renderCamera.projectionMatrix );
+		this.textureMatrix.multiply( this.renderCamera.matrixWorldInverse );
+
+	}
 }
